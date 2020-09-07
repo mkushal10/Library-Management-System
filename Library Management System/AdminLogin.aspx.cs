31f0a76d83df9c2aa5fe988eb510732511c268b9
Kushal Neupane <mkushal10@gmail.com>
2020-09-07 21:26:41 +0545

    commit

--- Library Management System/AdminLogin.aspx.cs
+++ Library Management System/AdminLogin.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +12,7 @@ namespace Library_Management_System
 {
     public partial class AdminLogin : System.Web.UI.Page
     {
+        string strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
 
@@ -17,6 +21,41 @@ namespace Library_Management_System
         protected void Button1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+
+                }
+                SqlCommand cmd = new SqlCommand("select * from admin_login where username='" + TextBox1.Text.Trim() + "' AND password='" + TextBox2.Text.Trim() + "'", con);
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        Response.Write("<script>alert('Successful login');</script>");
+                        Session["username"] = dr.GetValue(0).ToString();
+                        Session["fullname"] = dr.GetValue(2).ToString();
+                        Session["role"] = "admin";
+                    }
+                    Response.Redirect("homepage.aspx");
+                }
+                else
+                {
+                    Response.Write("<script>alert('Invalid credentials');</script>");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+            }
         }
     }
 }
--- Library Management System/Site1.Master.cs
+++ Library Management System/Site1.Master.cs
@@ -11,7 +11,62 @@ namespace Library_Management_System
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            try
+            {
+                if (Session["role"].Equals(""))
+                {
+                    BtnUserLogin.Visible = true; // user login link button
+                    BtnSignUp.Visible = true; // sign up link button
 
+                    BtnLogout.Visible = false; // logout link button
+                    BtnUserName.Visible = false; // hello user link button
+
+
+                    BtnAdminLogin.Visible = true; // admin login link button
+                    BtnAuthorManagement.Visible = false; // author management link button
+                    BtnPublisherManagement.Visible = false; // publisher management link button
+                    BtnBookInventory.Visible = false; // book inventory link button
+                    BtnBookIssuing.Visible = false; // book issuing link button
+                    BtnMemberManagement.Visible = false; // member management link button
+
+                }
+                else if (Session["role"].Equals("user"))
+                {
+                    BtnUserLogin.Visible = false; // user login link button
+                    BtnSignUp.Visible = false; // sign up link button
+
+                    BtnLogout.Visible = true; // logout link button
+                    BtnUserName.Visible = true; // hello user link button
+                    BtnUserName.Text = "Hello " + Session["username"].ToString();
+
+
+                    BtnAdminLogin.Visible = true; // admin login link button
+                    BtnAuthorManagement.Visible = false; // author management link button
+                    BtnPublisherManagement.Visible = false; // publisher management link button
+                    BtnBookInventory.Visible = false; // book inventory link button
+                    BtnBookIssuing.Visible = false; // book issuing link button
+                }
+                else if (Session["role"].Equals("admin"))
+                {
+                    BtnUserLogin.Visible = false; // user login link button
+                    BtnSignUp.Visible = false; // sign up link button
+
+                    BtnLogout.Visible = true; // logout link button
+                    BtnUserName.Visible = true; // hello user link button
+                    BtnUserName.Text = "Hello Admin";
+
+
+                    BtnAdminLogin.Visible = false; // admin login link button
+                    BtnAuthorManagement.Visible = true; // author management link button
+                    BtnPublisherManagement.Visible = true; // publisher management link button
+                    BtnBookInventory.Visible = true; // book inventory link button
+                    BtnBookIssuing.Visible = true; // book issuing link button
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
 
         protected void BtnAdminLogin_Click(object sender, EventArgs e)
@@ -57,6 +112,28 @@ namespace Library_Management_System
         protected void BtnSignUp_Click(object sender, EventArgs e)
         {
             Response.Redirect("UserSignUp.aspx");
+        }
+
+        protected void BtnLogout_Click(object sender, EventArgs e)
+        {
+            Session["username"] = "";
+            Session["fullname"] = "";
+            Session["role"] = "";
+            Session["status"] = "";
+
+            BtnUserLogin.Visible = true; // user login link button
+            BtnSignUp.Visible = true; // sign up link button
+
+            BtnLogout.Visible = false; // logout link button
+            BtnUserName.Visible = false; // hello user link button
+
+
+            BtnAdminLogin.Visible = true; // admin login link button
+            BtnAuthorManagement.Visible = false; // author management link button
+            BtnPublisherManagement.Visible = false; // publisher management link button
+            BtnBookInventory.Visible = false; // book inventory link button
+            BtnBookIssuing.Visible = false; // book issuing link button
+            BtnMemberManagement.Visible = false; // member management link button
         }
     }
 }
